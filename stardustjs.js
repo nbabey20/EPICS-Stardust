9bcfdc9732aaaddff5fb0d123047210d97487912
nbabey20 <66934750+nbabey20@users.noreply.github.com>
2021-12-03 16:35:08 -0700

    Add files via upload

--- stardustjs.js
+++ stardustjs.js
@@ -2,32 +2,13 @@
 var irradiance = 0.0;
 var currentPower = 0.0;
 var maxPower = 0.0; 
-var calculate = document.getElementById("calculate"); 
+var degradation = 0.0; 
 
-//irradiance = document.getElementById("irr").value;
-//currentPower = document.getElementById("curr").value;
-//maxPower = document.getElementById("max").value; 
-
-
-calculate.addEventListener("click", onClick, false);
-
-document.getElementById("test").textContent = currentPower; 
-
-function onClick(e){
-    whatClicked = this.id;
-    if(whatClicked == "calculate"){
-        irradiance = document.getElementById("irr").value;
-        currentPower = document.getElementById("curr").value;
-        maxPower = document.getElementById("max").value;
-        calcDeg(irr, curr, max);
-    }
-}
-function calcDeg(irr, curr, max){
-    var stdIrr = 1000;
-    var operatingEff = ((stdIrr*curr)/(irr*max)) * 100;
-    var degredation = 100 - operatingEff;
-}
-var buttons = document.getElementsByClassName("btn");
-for(var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", onClick, false);
+function calcDeg(){
+    irradiance = document.getElementById("irr").value;
+    currentPower = document.getElementById("curr").value;
+    maxPower = document.getElementById("max").value;
+    degradation = 1 -((currentPower * 1000)/(maxPower * irradiance));
+    degradation = degradation.toFixed(3);
+    document.getElementById("degradation").innerHTML = degradation;
 }
